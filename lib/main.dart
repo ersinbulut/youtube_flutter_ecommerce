d258c10386148a72635c8d27c11de3744e7778cf
DEV <DEV@DESKTOP-8VP4IIT>
2024-09-24 17:47:07 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:grock/grock.dart';
+import 'package:youtube_ecommerce_app/constant/constant.dart';
+import 'package:youtube_ecommerce_app/view/splash.dart';
+
+void main() => runApp(const ProviderScope(child: MyApp()));
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Youtube ECommecne App',
+      debugShowCheckedModeBanner: false,
+      navigatorKey: Grock.navigationKey,
+      scaffoldMessengerKey: Grock.scaffoldMessengerKey,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Constant.white,
+      ),
+      home: const Splash(),
+    );
+  }
+}
---
+++ lib/view/base_scaffold.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_ecommerce_app/components/custom_appbar.dart';
+import 'package:youtube_ecommerce_app/components/custom_bottom_nav_bar.dart';
+import 'package:youtube_ecommerce_app/riverpod/base_scaffold_riverpod.dart';
+
+final baseScaffoldRiverpod =
+    ChangeNotifierProvider((ref) => BaseScaffoldRiverpod());
+
+class BaseScaffold extends ConsumerStatefulWidget {
+  const BaseScaffold({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _BaseScaffoldState();
+}
+
+class _BaseScaffoldState extends ConsumerState<BaseScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppbar(),
+      bottomNavigationBar: const CustomBottomNavBar(),
+      body: ref.watch(baseScaffoldRiverpod).body(),
+    );
+  }
+}
